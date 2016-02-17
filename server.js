f6da9b17650e54cc406159137310c78e32f82ff9
Tim <blargendarg@gmail.com>
2016-02-17 15:44:58 +0000

    got db connection working

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 'use strict';
 
 var express = require('express'),
-    app = express();
-
-app.get('/*', function(req, res) {
-    var response = {
+    app = express(),
+    mongoClient = require('mongodb').MongoClient;
+    
+    
+mongoClient.connect("mongodb://localhost:27017/clementinejs", function(err, db) {
+    if(err) {
+        console.error("Could not connect to DB!");
+    } else { 
+        console.log("Connected to DB!"); 
         
+        app.get('/*', function(req, res) {
+            var response = {
+            	"original url": req.url.substr(1),
+                "short url": "",
+                "db": db.collection('users').findOne({}),
+                "headers": req.headers
+            };
+            res.send(response);
+        });
     }
-    res.send(req.url);
 });
 
 
