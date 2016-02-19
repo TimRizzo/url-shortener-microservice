e7a843d122a9dc26fd8ee7b5596d28e1e7d6d4c7
Tim <blargendarg@gmail.com>
2016-02-19 17:38:13 +0000

    proper format adds param urls to database

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express'),
     mongo = require('mongodb').MongoClient,
     app = express(),
+    regex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/,
     urls = {};
         
 mongo.connect('mongodb://localhost:27017/urls', function(err, db) {
@@ -10,35 +11,45 @@ mongo.connect('mongodb://localhost:27017/urls', function(err, db) {
         console.log("Can't connect to DB!");
     } else {
         console.log("Connected to DB!");
-        
    
         app.use('/*', function(req, res) {
-          
-        db.collection('userCreated').find({ "original url": req.params['0'] }).toArray(function(err, result) {
-        if (err) {
-            throw err;
+            if(req.params['0'].match(regex)) {
+                db.collection('userCreated').find({ "original url": req.params['0'] }).limit(1).toArray(function(err, result) {
+                if (err) {
+                    throw err;
+                    }
+                if(result.length === 0) {
+                    db.collection('userCreated').insert(
+                        {
+                            "original url": req.params['0'],
+                            "short url": "http://url-shortener-microservice-timrizzo.c9users.io/short"
+                        });
+                    urls = {
+                    	"original url": req.params['0'],
+                        "short url": "",
+                        "db": "Not in DB!"
+                    };
+                } else {
+                    urls = {
+                    	"original url": req.params['0'],
+                        "short url": "",
+                        "db": result,
+                        "typeof result": typeof result
+                    };
+                }
+                res.send(urls);
+                });
+            } else {
+                res.send("Not proper URL!");
             }
-        res.send(result);
-        });
-        /*
-        urls = {
-        	"original url": req.url.substr(1),
-            "short url": "",
-            "db": db.collection('userCreated').findOne({"original url" : "http://testurl.com"})
-        };
-        
-        res.send(urls);
-        */
-        });
-
-        var port = process.env.PORT || 8080;
-        app.listen(port,  function () {
-        	console.log('Node.js listening on port ' + port + '...');
         });
     }
 });
         
-    
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
         
         
 
