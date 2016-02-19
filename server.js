05ef795727ee81530f325e1d4abed505e310c4ce
Tim <blargendarg@gmail.com>
2016-02-19 16:41:48 +0000

    got DB connected to output finally

--- server.js
+++ server.js
@@ -1,30 +1,46 @@
 'use strict';
 
 var express = require('express'),
+    mongo = require('mongodb').MongoClient,
     app = express(),
-    mongoClient = require('mongodb').MongoClient;
-    
-    
-mongoClient.connect("mongodb://localhost:27017/clementinejs", function(err, db) {
-    if(err) {
-        console.error("Could not connect to DB!");
-    } else { 
-        console.log("Connected to DB!"); 
+    urls = {};
         
-        app.get('/*', function(req, res) {
-            var response = {
-            	"original url": req.url.substr(1),
-                "short url": "",
-                "db": db.collection('users').findOne({}),
-                "headers": req.headers
-            };
-            res.send(response);
+mongo.connect('mongodb://localhost:27017/urls', function(err, db) {
+    if(err) {
+        console.log("Can't connect to DB!");
+    } else {
+        console.log("Connected to DB!");
+        
+   
+        app.use('/*', function(req, res) {
+          
+        db.collection('userCreated').find({ "original url": req.params['0'] }).toArray(function(err, result) {
+        if (err) {
+            throw err;
+            }
+        res.send(result);
+        });
+        /*
+        urls = {
+        	"original url": req.url.substr(1),
+            "short url": "",
+            "db": db.collection('userCreated').findOne({"original url" : "http://testurl.com"})
+        };
+        
+        res.send(urls);
+        */
+        });
+
+        var port = process.env.PORT || 8080;
+        app.listen(port,  function () {
+        	console.log('Node.js listening on port ' + port + '...');
         });
     }
 });
+        
+    
+        
+        
 
 
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
-});
+
